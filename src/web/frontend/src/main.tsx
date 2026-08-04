c2ad46aa19cca42231154b0b21eb1d7710e8109e
Mikolaj Manczak <mikomanczak@gmail.com>
2026-08-04 20:16:44 +0200

    skeleton for UI to enter keywords

--- src/web/frontend/src/main.tsx
+++ src/web/frontend/src/main.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(<h1>Hello World</h1>);
+createRoot(document.getElementById('root')!).render(<App />);
